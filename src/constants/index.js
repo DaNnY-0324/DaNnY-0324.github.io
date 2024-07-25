1455e0273b652fec722562058caccb98473f0524
Danny Nguyen GP <danny@govtportal.com>
2024-07-25 17:45:14 -0400

    Updated Projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,32 +9,37 @@ export const EXPERIENCES = [
     role: "Software Developer Engineer Intern",
     company: "Gov't Portal",
     description: `Developed new softwares and POS systems for multiple companies which increased the companies revenue by 25%. Delivered a comprehensive Park & Recs management system by collaborating with a team of 4 developers, resulting in a 40% increase in operational efficiency for municipal management.`,
-    technologies: ["php", "tailwindcss", "myphpadmin", "mySQL", "Git"],
+    technologies: ["php", "tailwindcss", "phpMyAdmin", "mySQL", "Git"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
-    description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
     title: "Portfolio Website",
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "TailwindCSS", "React"],
+  },
+  {
+    title: "Managment Dashboard Software",
+    description:
+      "Developed a web-based Membership Management Software using PHP and MySQL with a team of 4 developers, designed to automate registration, renewal, and verification processes. Implemented secure user authentication and QR code generation using TailwindCSS and Endroid QRCode library, enhancing user experience and data protection.",
+    technologies: ["php", "TailwindCSS", "mySQL", "phpMyAdmin"],
+  },
+  {
+    title: "Dice to 100",
+    description:
+      "Dice to 100 is a simple and fun dice game for two players. The objective of the game is to be the first player to reach or exceed 100 points.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Guess My Number",
+    description: `"Guess My Number!" is a simple number guessing game. The game generates a random secret number between 1 and 20, and the player has to guess the number. `,
+    technologies: ["HTML", "CSS", "JavaScript"],
   },
 ];
 
 export const CONTACT = {
-  phoneNo: "",
-  email: "",
+  phoneNo: "(404) 729-9501",
+  email: "dannynguyen032@gmail.com",
 };
